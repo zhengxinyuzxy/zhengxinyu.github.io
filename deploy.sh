f38e9c8d6e7dda4e4046520946018dad564c3451
zhengxinyu <mellowzz@163.com>
2023-07-19 17:55:44 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/zhengxinyuzxy/blogs.git master:gh-pages
+git push -f https://github.com/zhengxinyuzxy/zhengxinyuzxy.github.io.git master:github-pages
 
 cd -
